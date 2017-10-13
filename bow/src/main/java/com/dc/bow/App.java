b5fac97fef54dd1f12eae94d4bf849b5b5bbfa9f
Digital Alchemist <developer.anwar@gmail.com>
2017-10-12 23:26:22 -0400

    Project Bow
    A file compare utility to match one or more files using sequential and
    parallel execution techniques using multi-threading

---
+++ bow/src/main/java/com/dc/bow/App.java
@@ -0,0 +1,13 @@
+package com.dc.bow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
